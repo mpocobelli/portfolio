07446a50992c1c1b686d7dbd0ae80be57edcef1f
mpocobelli <mpocobelli@gmail.com>
2022-06-18 23:01:19 +0200

    portfolio 2.0
    I change the design and update bootstrap. Enjoy!

--- js/fullscreen-menu.js
+++
@@ -1,7 +0,0 @@
-$('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('#overlay').toggleClass('open');
-   $('nav li a').on('click', function(){
-   $('#toggle').trigger('click');
-});
-  });
